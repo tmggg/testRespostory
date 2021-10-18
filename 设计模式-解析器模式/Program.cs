f7c2b086aebd69a92c3e14e653ecbdb78f562159
TMGG <tm574378328@gmail.com>
2021-10-18 23:49:59 +0800

    Revert "更新"
    This reverts commit c3196113d4ae0033a65fe66c04c9e5e00989b392.

--- 设计模式-解析器模式/Program.cs
+++ 设计模式-解析器模式/Program.cs
@@ -23,11 +23,10 @@ namespace 设计模式_解析器模式
             MethodInfo method = assembly.GetType("设计模式_解析器模式." + context.Output.Split('.')[0]).GetMethod(context.Output.Split('.')[1].Replace("()", ""));
             object obj = method.Invoke(null, null);
             List<MessageModel> m = (List<MessageModel>)obj;
-            //
             Console.WriteLine("中文语法：" + chinese + "\r\n");
             Console.WriteLine("解释后的C#代码：" + context.Output + "\r\n");
             Console.WriteLine("执行结果：" + m[0].Message + " " + m[0].PublishTime.ToString() + "\r\n");
-            //
+
         }
     }
 }
