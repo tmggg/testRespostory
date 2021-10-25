f197e111cbd2eb88cca925680e7bfe83bdd76fc2
TMGG <tm574378328@gmail.com>
2021-10-25 17:53:47 +0800

    c

--- 设计模式/Program.cs
+++ 设计模式/Program.cs
@@ -42,8 +42,8 @@ namespace 设计模式
                     m.RestoreMemento(mmc.MessageModel);
                     m.PublishTime = DateTime.Now;
                 }
-                //bb
-                //cc
+                //bbb
+                //ccc
             }
 
 
