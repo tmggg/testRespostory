3d94a66ada39a58417e64ad3b788fec52f35f14f
TMGG <tm574378328@gmail.com>
2022-05-24 20:00:56 +0800

    c1

--- 设计模式/Program.cs
+++ 设计模式/Program.cs
@@ -22,11 +22,6 @@ namespace 设计模式
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-            //master
-=======
->>>>>>> c1
-            //c1
             SqlMessage m = new SqlMessage();
             ///
             ///
